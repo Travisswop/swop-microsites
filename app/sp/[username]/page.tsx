4bf6834a6be974843a3da60dc3e22bb0acd515c5
irakibul7 <irakibul568@gmail.com>
2023-11-15 12:31:03 +0600

    update assetlinksjson file

--- app/sp/[username]/page.tsx
+++ app/sp/[username]/page.tsx
@@ -108,6 +108,8 @@ export default async function PublicProfile({ params }: PageProps) {
     gatedInfo,
   } = data;
 
+  console.log(data);
+
   if (!gatedAccess && direct) {
     return (
       <>
